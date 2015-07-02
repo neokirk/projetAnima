12f9cc3c16476a8b8556c75c72b2678ecf208773
neokirk <neokirk@gmail.com>
2015-07-02 21:45:40 +0200

    ajout des règles de combat à distance

--- src/animaJDR/Valeurs.java
+++ src/animaJDR/Valeurs.java
@@ -3,37 +3,44 @@ package animaJDR;
 public class Valeurs
 {
 	// Initiative
-	static final int malusInit01 = -125 ;
-	static final int malusInit02 = -100 ;
-	static final int malusInit03 = -75 ;
+	public static final int malusInit01 = -125 ;
+	public static final int malusInit02 = -100 ;
+	public static final int malusInit03 = -75 ;
 	
 	// Situations de combat
-	static final int malusSurprise = -90 ;
-	static final int malusDefFlanc = -30 ;
-	static final int malusDefDos = -80 ;
-	static final int malusParCecitePartielle = -30 ;
-	static final int malusEscCecitePartielle = -15 ;
-	static final int malusDefCeciteTotale = -80 ;
-	static final int malusParParalysieLegere = -20 ;
-	static final int malusEscParalysieLegere = -40 ;
-	static final int malusDefParalysiePartielle = -80 ;
-	static final int malusDefParalysieTotale = -20 ;
+	public static final int malusSurprise = -90 ;
+	public static final int malusDefFlanc = -30 ;
+	public static final int malusDefDos = -80 ;
+	public static final int malusParCecitePartielle = -30 ;
+	public static final int malusEscCecitePartielle = -15 ;
+	public static final int malusDefCeciteTotale = -80 ;
+	public static final int malusParParalysieLegere = -20 ;
+	public static final int malusEscParalysieLegere = -40 ;
+	public static final int malusDefParalysiePartielle = -80 ;
+	public static final int malusDefParalysieTotale = -20 ;
 	
-	// Attaques visées
-	static final int attaqueCou = -80 ;
-	static final int attaqueTete = -60 ;
-	static final int attaqueCoude = -60 ;
-	static final int attaqueCoeur = -60 ;
-	static final int attaqueAine = -60 ;
-	static final int attaquePied = -50 ;
-	static final int attaqueMain = -40 ;
-	static final int attaqueGenou = -40 ;
-	static final int attaqueAbdomen = -20 ;
-	static final int attaqueBras = -20 ;
-	static final int attaqueCuisse = -20 ;
-	static final int attaqueTibia = -10 ;
-	static final int attaqueTorse = -10 ;
-	static final int attaqueOeil = -100 ;
-	static final int attaquePoignet = -40 ;
-	static final int attaqueEpaule = -30 ;
+	// combat a distance
+	public static final int esquiveTir = -30 ;
+	public static final int paradeTir = -80 ;
+	public static final int paradeLance = -50 ;
+	public static final int paradeTirBouclier = -30 ;
+	public static final int paradeTirMaitre = -20 ;
+	
+	// Attaques visees
+	public static final int attaqueCou = -80 ;
+	public static final int attaqueTete = -60 ;
+	public static final int attaqueCoude = -60 ;
+	public static final int attaqueCoeur = -60 ;
+	public static final int attaqueAine = -60 ;
+	public static final int attaquePied = -50 ;
+	public static final int attaqueMain = -40 ;
+	public static final int attaqueGenou = -40 ;
+	public static final int attaqueAbdomen = -20 ;
+	public static final int attaqueBras = -20 ;
+	public static final int attaqueCuisse = -20 ;
+	public static final int attaqueTibia = -10 ;
+	public static final int attaqueTorse = -10 ;
+	public static final int attaqueOeil = -100 ;
+	public static final int attaquePoignet = -40 ;
+	public static final int attaqueEpaule = -30 ;
 }
